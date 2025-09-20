75de5bf2325fe8568550fd41b8fd2dd199932113
quantum-corn <akashkhetan044@gmail.com>
2025-09-20 22:14:03 +0530

    Bug: DELETE & POST not working on deployed version
    Exploring the code to figure out the issue.

--- frontend/app/api-handler.ts
+++ frontend/app/api-handler.ts
@@ -3,16 +3,16 @@ import { redirect } from "next/navigation";
 const url = process.env.NEXT_PUBLIC_URL as string;
 
 export async function getTasks() {
-  return await fetch(url).then((res) => res.json());
+  return await fetch(`${url}/`).then((res) => res.json());
   /* TODO Add toasts */
 }
 
 export async function getTask(id: string) {
-  return await fetch(`${url}${id}/`).then((res) => res.json());
+  return await fetch(`${url}/${id}`).then((res) => res.json());
 }
 
 export async function deleteTask(id: string) {
-  await fetch(`${url}${id}/`, {
+  await fetch(`${url}/${id}`, {
     method: "DELETE",
   });
   /* TODO Add toasts */
