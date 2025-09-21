054be9614381fc99fdf5d60ba0666a7fb9a4f501
quantum-corn <akashkhetan044@gmail.com>
2025-09-21 10:56:24 +0530

    Fix: Fix the CORS configuration correctly

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[os.getenv("FRONTEND_URL")],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
