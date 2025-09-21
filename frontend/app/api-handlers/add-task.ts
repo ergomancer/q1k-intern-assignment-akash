22e63deb2402ec00382f4cf6fb7dba50faabc96d
quantum-corn <akashkhetan044@gmail.com>
2025-09-21 17:58:37 +0530

    Bug: Trying to explore POST request issue

--- frontend/app/api-handlers/add-task.ts
+++ frontend/app/api-handlers/add-task.ts
@@ -6,13 +6,8 @@ const url = process.env.NEXT_PUBLIC_URL as string;
 
 export async function postAddTask(data: FormData) {
   "use server";
-  console.log("Adding task");
-  console.log("FormData:\n", data);
   let newData = Object.fromEntries(data);
-  console.log("Parsed FormData:\n", newData);
   if (!newData.due_date) delete newData.due_date;
-  console.log("Final Data:\n", newData);
-  console.log("Posting to:", url);
   await fetch(`${url}/`, {
     method: "POST",
     body: JSON.stringify(newData),
@@ -20,14 +15,7 @@ export async function postAddTask(data: FormData) {
     headers: {
       "Content-type": "application/json",
     },
-  })
-    .then((res) => {
-      console.log("Response:", res);
-    })
-    .catch((err) => {
-      console.error("Error:", err);
-    });
-  console.log("API interaction complete\nRedirecting to /tasks");
+  });
   /*TODO Add toasts */
   redirect("/tasks");
 }
